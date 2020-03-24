23663e2e887e39a973578d243449149784a85976
lee <topkids5326@gmail.com>
2020-03-24 17:10:33 +0900

    tue

--- 3/23일/23_practice_3.cpp
+++ 3/23일/23_practice_3.cpp
@@ -7,7 +7,7 @@ int main(){
     int a;
     cout << "countdown: \n";
     for (int i = 10; i>0; --i){
-        cout << i << '\n';
+        cout << i << '\n'; //자동으로 flush 해버림.
         this_thread::sleep_for (chrono::seconds(1));
     }
 
