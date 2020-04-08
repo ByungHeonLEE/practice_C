dfefe40909d0148853fd7b087aa45201eaaaaaad
이병헌 <topkids5326@gmail.com>
2020-04-08 19:54:51 +0900

    s

--- 4/3일/cpp.cpp
+++ 4/3일/cpp.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "StaticSample.h"
+
+using namespace std;
+
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    double Numbers[] = {12.55, 94.68, 8.18, 60.37, 104.502, 75.05};
+    int Count = sizeof(Numbers) / sizeof(double);
+
+    double Total = Sum(Numbers, Count);
+    double Avg = Average(Numbers, Count);
+    double Low = Min(Numbers, Count);
+    double High = Max(Numbers, Count);
+
+    cout << "Characteristics of the list";
+    cout << "\nMinimum" << Low;
+    cout << "\nMaximum" << High;
+    cout << "\nTotal: " << Total;
+    cout << "\nAverage: " << Avg << endl;
+    return 0;
 };
